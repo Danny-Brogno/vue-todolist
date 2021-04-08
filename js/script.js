f524f2618adc88d5ad4834564b1166e8f759f805
Danny <danny.brogno@gmail.com>
2021-04-08 16:39:40 +0200

    Vue done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function numberGenerator(min, max) {
   let localMin = min;
   let localMax = max - min + 1;
   return Math.floor(Math.random() * (localMax - localMin)) + localMin;
+
 } // END OF FUNCTION numberGenerator
 
 // -----------------------------------------------------------------------------
@@ -14,13 +15,33 @@ function vueFunction() {
     el: "#dannyVue",
     data: {
       "url": "img/boolean-logo.png",
+
+      "todos": [
+        "to-do 1",
+        "to-do 2",
+        "to-do 10",
+        "to-do 7",
+      ],
+
+      newTodo: ""
     }, // END OF DATA
 
     methods: {
 
-    }, // END OF METHODS
+      createNewToDo: function() {
 
-  }); // END OF VUE
+        if(this.newTodo.length > 0) {
+          this.todos.push(this.newTodo);
+          this.newTodo = " ";
+        } // END OF IF
+
+      }, // END OF createNewToDo FUNCTION
+      clickDeleteTodo: function(index) {
+        this.todos.splice(index, 1);
+      }
+    } // END OF METHODS
+
+  }); // END OF NEW VUE
 
 } // END OF FUNCTION vueFunction
 
@@ -30,5 +51,5 @@ function init () {
   vueFunction();
 }
 
-$(init);
+document.addEventListener("DOMContentLoaded", init);
 // -----------------------------------------------------------------------------
